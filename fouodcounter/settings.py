024865b76e3a991e4b143d0542de58e86abeccd6
Aditya singh <115808044+Adityadeveloper28@users.noreply.github.com>
2023-12-12 11:03:35 +0530

    Update settings.py

--- fouodcounter/settings.py
+++ fouodcounter/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ypqtpi@@90vw^8x183$@$ogbuyrx-wec7sqcluqjun2+$2jxw0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
